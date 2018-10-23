d4e5075eff829753fae25bc146c3bdace0fdbfbf
(s) Mike Austin <maustin@pm7-0277.uopnet.plymouth.ac.uk>
2018-10-23 14:47:08 +0100

    Sample

---
+++ 00_Template/sketch.js
@@ -0,0 +1,26 @@
+//https://github.com/stavrosdidakis/DAT-GAD-405_2017
+//https://p5js.org
+
+//DAT405 / GAD405
+//00_Template
+//The purpose of this sketch is to provide a basic template
+//for P5.js projects
+
+//Initialization function
+function setup() {
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Initialization: OK")
+  createCanvas(500,500);
+}
+
+//Rendering function
+function draw() {
+  //Set a background color
+  background(255, 0, 255);
+
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Rendering...")
+  noLoop();
+}

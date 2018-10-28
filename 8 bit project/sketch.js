deb7138301e9bb1a90d053b37c9263642f61af74
(s) Mike Austin <mike.austin@students.plymouth.ac.uk>
2018-10-28 09:30:11 +0000

    stav 8bit

--- 8 bit project/sketch.js
+++ 8 bit project/sketch.js
@@ -117,7 +117,7 @@ function draw() {
   fill("pink");
   rect(150, 40, 10, 10);
 
-  //5th line beard, ears
+  //5th line beard, nose
   fill("black");
   rect(40, 50, 10, 10);
   rect(50, 50, 10, 10);
@@ -136,8 +136,7 @@ function draw() {
   rect(140, 50, 10, 10);
   rect(150, 50, 10, 10);
 
-  //6th line
-
+  //6th line beard, mo
   rect(50, 60, 10, 10);
   rect(60, 60, 10, 10);
   rect(70, 60, 10, 10);
@@ -146,29 +145,28 @@ function draw() {
   rect(100, 60, 10, 10);
   rect(110, 60, 10, 10);
   rect(120, 60, 10, 10);
+
   fill("brown");
   rect(130, 60, 10, 10);
+
   fill("black");
   rect(140, 60, 10, 10);
 
-  //7th line
-
-
+  //7th line lips, beard
   rect(60, 70, 10, 10);
   rect(70, 70, 10, 10);
+
   fill("red");
   rect(80, 70, 10, 10);
   rect(90, 70, 10, 10);
   rect(100, 70, 10, 10);
   rect(110, 70, 10, 10);
+
   fill("black");
   rect(120, 70, 10, 10);
   rect(130, 70, 10, 10);
 
-  //8th line
-
-
-
+  //8th line beard
   rect(70, 80, 10, 10);
   rect(80, 80, 10, 10);
   rect(90, 80, 10, 10);
@@ -176,44 +174,50 @@ function draw() {
   rect(110, 80, 10, 10);
   rect(120, 80, 10, 10);
 
-  //9th line
-
+  //9th line beard.
   rect(80, 90, 10, 10);
   rect(90, 90, 10, 10);
   rect(100, 90, 10, 10);
   rect(110, 90, 10, 10);
 
 
-  //10th line
+  //10th line neck, shirt, beard.
   fill("pink");
   rect(80, 100, 10, 10);
+
   fill("white");
   rect(70, 100, 10, 10);
+
   fill("black");
   rect(90, 100, 10, 10);
   rect(100, 100, 10, 10);
+
   fill("pink");
   rect(110, 100, 10, 10);
+
   fill("white");
   rect(120, 100, 10, 10);
 
 
-  //11th line
+  //11th line shirt, neck.
   fill("white");
   rect(60, 110, 10, 10);
   rect(70, 110, 10, 10);
+
   fill("pink");
   rect(80, 110, 10, 10);
   rect(90, 110, 10, 10);
   rect(100, 110, 10, 10);
   rect(110, 110, 10, 10);
+
   fill("white");
   rect(120, 110, 10, 10);
   rect(130, 110, 10, 10);
 
-  //12th line
+  //12th line arms, shirt
   fill("pink");
   rect(60, 120, 10, 10);
+
   fill("white");
   rect(70, 120, 10, 10);
   rect(80, 120, 10, 10);
@@ -221,13 +225,15 @@ function draw() {
   rect(100, 120, 10, 10);
   rect(110, 120, 10, 10);
   rect(120, 120, 10, 10);
+
   fill("pink");
   rect(130, 120, 10, 10);
 
 
-  //13th line
+  //13th line srms shirt
   fill("pink");
   rect(60, 130, 10, 10);
+
   fill("white");
   rect(70, 130, 10, 10);
   rect(80, 130, 10, 10);
@@ -235,12 +241,12 @@ function draw() {
   rect(100, 130, 10, 10);
   rect(110, 130, 10, 10);
   rect(120, 130, 10, 10);
+
   fill("pink");
   rect(130, 130, 10, 10);
 
 
   //14th line bottom of t-shirt
-
   fill("white");
   rect(70, 140, 10, 10);
   rect(80, 140, 10, 10);
@@ -251,7 +257,6 @@ function draw() {
 
 
   //15th line start of trousers
-
   fill("blue");
   rect(70, 150, 10, 10);
   rect(80, 150, 10, 10);
@@ -261,24 +266,23 @@ function draw() {
   rect(120, 150, 10, 10);
 
   //16th line trousers 2nd line
-
   rect(70, 160, 10, 10);
   rect(80, 160, 10, 10);
+
   fill("blue");
   rect(110, 160, 10, 10);
   rect(120, 160, 10, 10);
 
   //17th line trousers 3rd line
-
   rect(70, 170, 10, 10);
   rect(80, 170, 10, 10);
+
   fill("blue");
   rect(110, 170, 10, 10);
   rect(120, 170, 10, 10);
 
 
   //18th line shoes
-
   fill("black");
   rect(60, 180, 10, 10);
   rect(70, 180, 10, 10);
@@ -286,6 +290,7 @@ function draw() {
   rect(110, 180, 10, 10);
   rect(120, 180, 10, 10);
   rect(130, 180, 10, 10);
+  
   console.log("Searching...")
   noLoop();
 }

085f6379602b058df60ac35ebb61a7be6204d2a8
(s) Mike Austin <mike.austin@students.plymouth.ac.uk>
2018-10-28 09:14:39 +0000

    Stav 8 bit

--- 8 bit project/sketch.js
+++ 8 bit project/sketch.js
@@ -8,197 +8,264 @@
 
 //Initialization function
 function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Initialization: Mad House")
-  createCanvas(500,500);
+
+  createCanvas(200, 200);
+  background(255, 0, 255);
 }
-
-//Rendering function
+//top layer of rect
 function draw() {
-  //Set a background color
-  background(0, 255, 255);
-  //2nd line from top
 
-
-  rect(25, 2, 15, 15);
-
-  rect(40, 2, 15, 15);
-
-  rect(55, 2, 15, 15);
-
-  rect(70, 2, 15, 15);
-
-  rect(85, 2, 15, 15);
-
-
-
-//3rd line from top
-  rect(10, 17, 15, 15);
-
-  rect(25, 17, 15, 15);
-
-  rect(40, 17, 15, 15);
-
-  rect(55, 17, 15, 15);
-
-  rect(70, 17, 15, 15);
-
-  rect(85, 17, 15, 15);
-
-  rect(100, 17, 15, 15);
-
-
-
-//4th line from top
-rect(10, 32, 15, 15);
-
-rect(25, 32, 15, 15);
-
-rect(40, 32, 15, 15);
-
-rect(55, 32, 15, 15);
-
-rect(70, 32, 15, 15);
-
-rect(85, 32, 15, 15);
-
-rect(100, 32, 15, 15);
-
-//5th line from top
-  rect(4, 5, 0.1, 0.1);
-
-  rect(5, 5, 0.1, 0.1);
-
-  rect(6, 5, 0.1, 0.1);
-
-  rect(7, 5, 0.1, 0.1);
-
-  rect(8, 5, 0.1, 0.1);
-
-  rect(9, 5, 0.1, 0.1);
-
-  rect(10, 5, 0.1, 0.1);
-
-  rect(11, 5, 0.1, 0.1);
-
-  rect(12, 5, 0.1, 0.1);
-
-  rect(13, 5, 0.1, 0.1);
-
-  rect(14, 5, 0.1, 0.1);
-
-  rect(15, 5, 0.1, 0.1);
-
-  //6th line.
-  rect(3, 6, 0.1, 0.1);
-
-  rect(4, 6, 0.1, 0.1);
-
-  rect(5, 6, 0.1, 0.1);
-
-  rect(6, 6, 0.1, 0.1);
-
-  rect(7, 6, 0.1, 0.1);
-
-  rect(8, 6, 0.1, 0.1);
-
-  rect(9, 6, 0.1, 0.1);
-
-  rect(10, 6, 0.1, 0.1);
-
-  rect(11, 6, 0.1, 0.1);
-
-  rect(12, 6, 0.1, 0.1);
-
-  rect(13, 6, 0.1, 0.1);
-
-  rect(14, 6, 0.1, 0.1);
-
-  rect(15, 6, 0.1, 0.1);
-
-  rect(16, 6, 0.1, 0.1);
-
-  //7th line.
-  rect(4, 7, 0.1, 0.1);
-
-  rect(5, 7, 0.1, 0.1);
-
-  rect(6, 7, 0.1, 0.1);
-
-  rect(7, 7, 0.1, 0.1);
-
-  rect(8, 7, 0.1, 0.1);
-
-  rect(9, 7, 0.1, 0.1);
-
-  rect(10, 7, 0.1, 0.1);
-
-  rect(11, 7, 0.1, 0.1);
-
-  rect(12, 7, 0.1, 0.1);
-
-  rect(13, 7, 0.1, 0.1);
-
-  rect(14, 7, 0.1, 0.1);
-
-  rect(15, 7, 0.1, 0.1);
-
-  //8th line.
-  rect(3, 8, 0.1, 0.1);
-
-  rect(4, 8, 0.1, 0.1);
-
-  rect(5, 8, 0.1, 0.1);
-
-  rect(6, 8, 0.1, 0.1);
-
-  rect(7, 8, 0.1, 0.1);
-
-  rect(8, 8, 0.1, 0.1);
-
-  rect(9, 8, 0.1, 0.1);
-
-  rect(10, 8, 0.1, 0.1);
-
-  rect(11, 8, 0.1, 0.1);
-
-  rect(12, 8, 0.1, 0.1);
-
-  rect(13, 8, 0.1, 0.1);
-
-  rect(14, 8, 0.1, 0.1);
-
-  rect(15, 8, 0.1, 0.1);
-
-  rect(16, 8, 0.1, 0.1);
-
-  //9th line.
-  rect(4, 9, 0.1, 0.1);
-
-  rect(5, 9, 0.1, 0.1);
-
-  rect(6, 9, 0.1, 0.1);
-
-  rect(7, 9, 0.1, 0.1);
-
-  rect(8, 9, 0.1, 0.1);
-
-  rect(9, 9, 0.1, 0.1);
-
-  rect(10, 9, 0.1, 0.1);
-
-  rect(11, 9, 0.1, 0.1);
-
-  rect(12, 9, 0.1, 0.1);
-
-  rect(13, 9, 0.1, 0.1);
-
-  rect(14, 9, 0.1, 0.1);
-
-  rect(15, 9, 0.1, 0.1);
-  //Print a message to the console - to view using Chrome:
-  //> Developer > Developer Tools > Console
+  stroke(0, 0);
+
+
+
+  //1st line
+  fill("black");
+  rect(60, 10, 10, 10);
+  rect(70, 10, 10, 10);
+  rect(80, 10, 10, 10);
+  rect(90, 10, 10, 10);
+  rect(100, 10, 10, 10);
+  rect(110, 10, 10, 10);
+  rect(120, 10, 10, 10);
+  rect(130, 10, 10, 10);
+
+
+
+  //2nd line
+
+
+  fill("black");
+  rect(50, 20, 10, 10);
+  fill("white");
+  rect(60, 20, 10, 10);
+  rect(70, 20, 10, 10);
+  rect(80, 20, 10, 10);
+  fill("black");
+  rect(90, 20, 10, 10);
+  rect(100, 20, 10, 10);
+  fill("white");
+  rect(110, 20, 10, 10)
+  rect(120, 20, 10, 10);
+  fill("white");
+  rect(130, 20, 10, 10);
+  fill("black");
+  rect(140, 20, 10, 10);
+
+
+
+
+  //3rd line
+  fill("pink");
+  rect(40, 30, 10, 10);
+  fill("black");
+  rect(50, 30, 10, 10);
+  fill("white");
+  rect(60, 30, 10, 10);
+  fill("brown");
+  rect(70, 30, 10, 10);
+  fill("white");
+  rect(80, 30, 10, 10);
+  fill("pink");
+  rect(90, 30, 10, 10);
+  rect(100, 30, 10, 10);
+  fill("white");
+  rect(110, 30, 10, 10);
+  fill("brown");
+  rect(120, 30, 10, 10);
+  fill("white");
+  rect(130, 30, 10, 10);
+  fill("black");
+  rect(140, 30, 10, 10);
+  fill("pink");
+  rect(150, 30, 10, 10);
+
+
+  //4th line
+  rect(40, 40, 10, 10);
+  fill("black");
+  rect(50, 40, 10, 10);
+  rect(60, 40, 10, 10);
+  rect(70, 40, 10, 10);
+  fill("pink");
+  rect(80, 40, 10, 10);
+  rect(90, 40, 10, 10);
+  rect(100, 40, 10, 10);
+  rect(110, 40, 10, 10);
+  fill("black");
+  rect(120, 40, 10, 10);
+  rect(130, 40, 10, 10);
+  rect(140, 40, 10, 10);
+  fill("pink");
+  rect(150, 40, 10, 10);
+
+  //5th line
+  fill("black");
+  rect(40, 50, 10, 10);
+  rect(50, 50, 10, 10);
+  rect(60, 50, 10, 10);
+  rect(70, 50, 10, 10);
+  rect(80, 50, 10, 10);
+  fill("pink");
+  rect(90, 50, 10, 10);
+  rect(100, 50, 10, 10);
+  fill("black");
+  rect(110, 50, 10, 10);
+  rect(120, 50, 10, 10);
+  rect(130, 50, 10, 10);
+  rect(140, 50, 10, 10);
+  rect(150, 50, 10, 10);
+
+  //6th line
+
+  rect(50, 60, 10, 10);
+  rect(60, 60, 10, 10);
+  rect(70, 60, 10, 10);
+  rect(80, 60, 10, 10);
+  rect(90, 60, 10, 10);
+  rect(100, 60, 10, 10);
+  rect(110, 60, 10, 10);
+  rect(120, 60, 10, 10);
+  fill("brown");
+  rect(130, 60, 10, 10);
+  fill("black");
+  rect(140, 60, 10, 10);
+
+  //7th line
+
+
+  rect(60, 70, 10, 10);
+  rect(70, 70, 10, 10);
+  fill("red");
+  rect(80, 70, 10, 10);
+  rect(90, 70, 10, 10);
+  rect(100, 70, 10, 10);
+  rect(110, 70, 10, 10);
+  fill("black");
+  rect(120, 70, 10, 10);
+  rect(130, 70, 10, 10);
+
+  //8th line
+
+
+
+  rect(70, 80, 10, 10);
+  rect(80, 80, 10, 10);
+  rect(90, 80, 10, 10);
+  rect(100, 80, 10, 10);
+  rect(110, 80, 10, 10);
+  rect(120, 80, 10, 10);
+
+  //9th line
+
+  rect(80, 90, 10, 10);
+  rect(90, 90, 10, 10);
+  rect(100, 90, 10, 10);
+  rect(110, 90, 10, 10);
+
+
+  //10th line
+  fill("pink");
+  rect(80, 100, 10, 10);
+  fill("white");
+  rect(70, 100, 10, 10);
+  fill("black");
+  rect(90, 100, 10, 10);
+  rect(100, 100, 10, 10);
+  fill("pink");
+  rect(110, 100, 10, 10);
+  fill("white");
+  rect(120, 100, 10, 10);
+
+
+  //11th line
+  fill("white");
+  rect(60, 110, 10, 10);
+  rect(70, 110, 10, 10);
+  fill("pink");
+  rect(80, 110, 10, 10);
+  rect(90, 110, 10, 10);
+  rect(100, 110, 10, 10);
+  rect(110, 110, 10, 10);
+  fill("white");
+  rect(120, 110, 10, 10);
+  rect(130, 110, 10, 10);
+
+  //12th line
+  fill("pink");
+  rect(60, 120, 10, 10);
+  fill("white");
+  rect(70, 120, 10, 10);
+  rect(80, 120, 10, 10);
+  rect(90, 120, 10, 10);
+  rect(100, 120, 10, 10);
+  rect(110, 120, 10, 10);
+  rect(120, 120, 10, 10);
+  fill("pink");
+  rect(130, 120, 10, 10);
+
+
+  //13th line
+  fill("pink");
+  rect(60, 130, 10, 10);
+  fill("white");
+  rect(70, 130, 10, 10);
+  rect(80, 130, 10, 10);
+  rect(90, 130, 10, 10);
+  rect(100, 130, 10, 10);
+  rect(110, 130, 10, 10);
+  rect(120, 130, 10, 10);
+  fill("pink");
+  rect(130, 130, 10, 10);
+
+
+  //14th line bottom of t-shirt
+
+  fill("white");
+  rect(70, 140, 10, 10);
+  rect(80, 140, 10, 10);
+  rect(90, 140, 10, 10);
+  rect(100, 140, 10, 10);
+  rect(110, 140, 10, 10);
+  rect(120, 140, 10, 10);
+
+
+  //15th line start of trousers
+
+  fill("blue");
+  rect(70, 150, 10, 10);
+  rect(80, 150, 10, 10);
+  rect(90, 150, 10, 10);
+  rect(100, 150, 10, 10);
+  rect(110, 150, 10, 10);
+  rect(120, 150, 10, 10);
+
+  //16th line trousers 2nd line
+
+  rect(70, 160, 10, 10);
+  rect(80, 160, 10, 10);
+  fill("blue");
+  rect(110, 160, 10, 10);
+  rect(120, 160, 10, 10);
+
+  //17th line trousers 3rd line
+
+  rect(70, 170, 10, 10);
+  rect(80, 170, 10, 10);
+  fill("blue");
+  rect(110, 170, 10, 10);
+  rect(120, 170, 10, 10);
+
+
+  //18th line shoes
+
+  fill("black");
+  rect(60, 180, 10, 10);
+  rect(70, 180, 10, 10);
+  rect(80, 180, 10, 10);
+  rect(110, 180, 10, 10);
+  rect(120, 180, 10, 10);
+  rect(130, 180, 10, 10);
   console.log("Searching...")
   noLoop();
 }

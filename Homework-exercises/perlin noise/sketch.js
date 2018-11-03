894530b1f85646bf9b63d9d4c9b231a2e96c57b4
(s) Mike Austin <mike.austin@students.plymouth.ac.uk>
2018-11-03 11:36:11 +0000

    new

--- Homework-exercises/perlin noise/sketch.js
+++ Homework-exercises/perlin noise/sketch.js
@@ -1,6 +1,6 @@
 
 var xoff1 = 0;
-var xoff2 = 100;
+
 
 function setup() {
   createCanvas(594, 841);
@@ -13,13 +13,12 @@ function draw() {
   background(51);
 
   var x = map(noise(xoff1), 0, 1, 0, width);
-  var y = map(noise(xoff2), 0, 1, 0, height);
+
 
   xoff1 += 0.02;
-  xoff2 += 0.02;
 
 
-  ellipse(x, y, 40, 40);
+  ellipse(x, 100, 40, 40);
 
 
 }

b31f7c4a9df30b2a5ea9514b9943da0d651e63e9
(s) Mike Austin <mike.austin@students.plymouth.ac.uk>
2018-11-08 12:55:48 +0000

    new

--- Homework-exercises/random shapes/sketch.js
+++ Homework-exercises/random shapes/sketch.js
@@ -1,6 +1,8 @@
 //Global variable for the size
 let size;
 let size2;
+let r;
+let e;
 function setup() {
   createCanvas(500, 500);
   frameRate(20); //Drawing speed 30 fps
@@ -11,6 +13,8 @@ function draw() {
   //Create a new random number for every frame (0 to 200 pixels)
   size = random(200);
   size2 = random(250);
+  r = rect(random(500), random(500), size, size);
+  e = ellipse(random(500), random(500), size, size);
   //Call the customShape function
   customShape();
 }
@@ -18,13 +22,6 @@ function draw() {
 //When the function is called, it will draw a shape with random position and color
 function customShape(){
   fill(random(255), 0, random(255), random(255));
-  ellipse(random(500), random(500), size, size);
-
-  if (key === 'a') {
-    stop();
-  } else if (key === 'b') {
-    fill(random(255), 0, random(255), random(255));
-    rect(random(500), random(500), size2, size2);
-  }
-
+  e;
+  r;
 }
